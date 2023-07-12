dd3cfae7379844157c02f5012323963c31e6d534
phoebe-cheng <chengfei621@gmail.com>
2023-07-12 10:21:53 +0800

    fix: fix node version 18 running error problem

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,24 @@ const {
   addLessLoader,
   // adjustStyleLoaders,
 } = require("customize-cra");
+const crypto = require('crypto');
+
+/**
+ * The MD4 algorithm is not available anymore in Node.js 17+ (because of library SSL 3).
+ * In that case, silently replace MD4 by the MD5 algorithm.
+ * 
+ * 备注：webpack5.61.0+以上修复了此问题，如升级webpack，则可移除此段代码
+ */
+try {
+  crypto.createHash('md4');
+} catch (e) {
+  console.warn('Crypto "MD4" is not supported anymore by this Node.js version');
+  const origCreateHash = crypto.createHash;
+  crypto.createHash = (alg, opts) => {
+    return origCreateHash(alg === 'md4' ? 'md5' : alg, opts);
+  };
+}
+
 const path = require("path");
 
 module.exports = override(
