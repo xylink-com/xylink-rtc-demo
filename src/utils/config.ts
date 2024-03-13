a95e0a6563ebe596d38867d4d6094866865533c9
luojinghui2 <luojinghui@xylink.com>
2024-03-13 20:47:21 +0800

    feat: upgrade version to v3.9.8 to support polling layout

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,7 +8,7 @@
 export const THIRD: boolean = true;
 
 // 服务地址，默认使用正式环境
-export const SERVER = 'st52webrtc.xylinkedu.com:8443';
+export const SERVER = 'cloudapi.xylink.com';
 
 // ************************此处配置您的开发者账号信息******************************
 // 默认提供了一组测试企业账号信息：
@@ -19,7 +19,7 @@ export const SERVER = 'st52webrtc.xylinkedu.com:8443';
 // ************************！！！第三方正式环境，需要替换对应自己企业的信息！！！***********************
 
 export const ACCOUNT = {
-  extId: "d7cbd71acd5811ed87f1525400c140b2",
-  clientId: "kEqrbDIrHEOwzzAuNx8xmZbC",
-  clientSecret: "E173GCxLkSudv8Bg3ey7EaNl7Y6SOr9q",
+  extId: "0142901e3d83e0a1e225ef92b8663fcaebda7242",
+  clientId: "BR1e5cptJgyC1aFRtiXfmdg3",
+  clientSecret: "I2aQ80XuXcrMTemfgwzm0zYvEyWpdqb0",
 };
